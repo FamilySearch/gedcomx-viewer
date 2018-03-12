10a6b8e2caa5ce63507b37b0011cda56e6efeed9
Randy Wilson <wilsonr@familysearch.org>
2018-03-12 14:30:52 -0600

    Display relatives.

--- src/gx-view.js
+++ src/gx-view.js
@@ -75,24 +75,79 @@ function makeNameHtml(name) {
   return s;
 }
 
-function makePersonHtml(person, doc) {
-  var s = "<div class='person' " + (person.id ? " id='" + person.id + "'" : "") + ">P" + person.index + " ";
-  s += makeGenderHtml(person.hasOwnProperty('gender') ? person.gender : null) + " ";
-  var i, j, k;
-  var name, nameForm, namePart;
-  var fact, pos, hadDate;
+/**
+ * Make HTML for a person in a GedcomX document.
+ * @param doc - GedcomX document that contains the person
+ * @param person - Person object (from within that GedcomX document) to use.
+ * @param idMap - Map of local person ID (p_1234567) to 1-based index (1, 2, 3...)
+ * @returns {string}
+ */
+function makePersonHtml(doc, person, idMap) {
+  var s = "<div class='person' " + (person.id ? " id='" + person.id + "'" : "") + ">";
+  s += makeGenderHtml(person.hasOwnProperty('gender') ? person.gender : null) + " P" + idMap[person.id] + ". ";
+
+  var i;
 
   if (person.hasOwnProperty('names')) {
     for (i = 0; i < person.names.length; i++) {
       s += makeNameHtml(person.names[i]);
     }
   }
-  else {
-    s += "<p class='name'>" + genderString + encode("<No name>") + "</p>\n";
-  }
 
+  s += getPersonFactsHtml(person);
+
+  //todo: fields
+
+  s += getRelativesHtml(doc, person, idMap);
+
+  //todo: isPrincipal
+  var identifier = getIdentifier(person);
+  s += "  <p class='ark'><a href='" + identifier +"'>" + identifier + "</a> (" + person.id + ")</p>\n";
+  s += "</div>\n"; // person div.
+  return s;
+}
+
+// Get the HTML for the list of a person's relatives.
+function getRelativesHtml(doc, person, idMap) {
+  var s = "";
+  var parentsAndSiblings = GedxPersonaPOJO.getParentsAndSiblings(doc, person);
+  var parentFamily, parent, parentType;
+  var spouseAndChildren, spouseFamily, spouseType;
+  var i, j;
+  for (i = 0; i < parentsAndSiblings.length; i++) {
+    parentFamily = parentsAndSiblings[i];
+    if (parentFamily.parents) {
+      for (j = 0; j < parentFamily.parents.length; j++) {
+        parent = parentFamily.parents[j];
+        parentType = parent.gender === "M" ? "Father" : (parent.gender === "F" ? "Mother" : "Parent");
+        s += relativeHtml(parentType, idMap[parent.id], parent.name);
+      }
+    }
+  }
+  spouseAndChildren = GedxPersonaPOJO.getSpousesAndChildren(doc, person);
+  for (i = 0; i < spouseAndChildren.length; i++) {
+    spouseFamily = spouseAndChildren[i];
+    if (spouseFamily.spouse) {
+      var spouse = spouseFamily.spouse;
+      spouseType = spouse.gender === "M" ? "Husband" : (spouse.gender === "F" ? "Wife" : "Spouse");
+      s += relativeHtml(spouseType, idMap[spouse.id], spouse.name);
+    }
+  }
+  return s;
+
+  function relativeHtml(relativeType, relativeIndex, relativeName) {
+    if (relativeType === undefined || relativeType === null) {
+      relativeType = '(Unknown relative type)';
+    }
+    return "<p class='relative'>" + relativeType + ": P" + relativeIndex + ". " + relativeName + "</p>";
+  }
+}
+
+function getPersonFactsHtml(person) {
+  var s = "";
+  var fact, pos, hadDate;
   if (person.hasOwnProperty('facts')) {
-    for (i = 0; i < person.facts.length; i++) {
+    for (var i = 0; i < person.facts.length; i++) {
       s += "<p class='fact'>";
       fact = person.facts[i];
       if (empty(fact.type)) {
@@ -117,12 +172,6 @@ function makePersonHtml(person, doc) {
       s += "</p>\n";
     }
   }
-  //todo: fields
-  //todo: relatives...
-  //todo: isPrincipal
-  var identifier = getIdentifier(person);
-  s += "  <p class='ark'><a href='" + identifier +"'>" + identifier + "</a> (" + person.id + ")</p>\n";
-  s += "</div>\n"; // person div.
   return s;
 }
 
@@ -160,14 +209,16 @@ function showRecord(url, doc) {
   //buildDocMaps(doc);
   var s = "<p>Record URL: " + url + "</p>\n";
   var i;
+  // Map of local person id (p_1234567) to index (1, 2, 3...)
+  var idMap = {};
 
   if (doc.hasOwnProperty('persons')) {
     // Create a short 1-based person index for viewing.
     for (i = 0; i < doc.persons.length; i++) {
-      doc.persons[i].index = i + 1;
+      idMap[doc.persons[i].id] = i + 1;
     }
     for (i = 0; i < doc.persons.length; i++) {
-      s += makePersonHtml(doc.persons[i], doc);
+      s += makePersonHtml(doc, doc.persons[i], idMap);
     }
   }
   if (doc.hasOwnProperty('relationships')) {
